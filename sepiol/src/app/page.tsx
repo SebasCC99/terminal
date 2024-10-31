e0df838a11a1c98c119b85617e6d8868526e0e7e
Sebastian Carazo <sebastian@lebas.dev>
2024-10-31 08:43:31 -0500

    create grid layout

--- sepiol/src/app/page.tsx
+++ sepiol/src/app/page.tsx
@@ -1,12 +1,15 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-    // https://tailwindcss.com/docs/container & https://tw-elements.com/learn/te-foundations/tailwind-css/containers/
-    <div className="container mx-auto grid min-h-80 grid-cols-3 gap-4 bg-slate-700">
-      <div className="col-start-2 flex items-center justify-center">
-        Hello terminal
-      </div>
+    <div className="container mx-auto grid min-h-80 grid-cols-5 grid-rows-4 gap-4 bg-slate-700">
+      <div className="row-span-full border">Sidebar</div>
+      <div className="col-span-full col-start-2 border">Navigation</div>
+      <div className="col-span-2 col-start-2 border">Data 1</div>
+      <div className="col-span-2 col-start-4 border">Data 2</div>
+      <div className="col-span-2 col-start-2 border">Data 3</div>
+      <div className="col-span-2 col-start-4 border">Data 4</div>
+      <div className="col-span-2 col-start-2 border">Data 5</div>
+      <div className="border">Data 6</div>
+      <div className="border">Data 7</div>
     </div>
   );
 }
